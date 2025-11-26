7a8ee07225393a74cd6a62d80d4d6068c1495dc9
nbogie <neillbogie@googlemail.com>
2025-11-26 14:41:48 +0000

    import "p5/global" instead of import "p5" in main, and we can cut the explicit import of global.d.ts from tsconfig - thanks davepagurek!

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-//this import RUNS p5 - it's not for type purposes - that's set up by  global.d.ts
-import "p5"; //TODO: remove this import when building for OP
+//TODO: remove this import when building for OP
+//imports the types for global-mode p5.  AND side-effects to start p5 running (calls our setup() etc)
+import "p5/global";
 
-//This import brings in the runtime p5 value (to reference p5.Vector.random2D() etc)
+//This import is still necessary for the p5 variable to be available at runtime.
 //@ts-ignore We want to keep this here even if the sketch doesn't currently use the p5 variable.
 import p5 from "p5";
 
+//just testing p5 variable is available
+p5.disableFriendlyErrors = false;
+
 window.setup = async function setup() {
     createCanvas(windowWidth, windowHeight);
+    console.log("window.p5 is", window.p5);
+    //                                 ^?
+    //just checking p5 variable is available (both to typescript and at runtime).
+    console.log(p5.Vector.random2D());
 };
 
 window.draw = function draw() {
