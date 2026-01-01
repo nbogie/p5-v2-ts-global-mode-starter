bfe980d86752eaa159de5075a40ce300dc2b9c0a
nbogie <neillbogie@googlemail.com>
2026-01-01 04:39:16 +0000

    add some use of p5 variable to main sketch just for testing of bundling

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import "p5/global";
 import p5 from "p5";
 import { randomColour } from "./utils/palette.ts";
 
+p5.disableFriendlyErrors = true;
+
 window.setup = function setup() {
   createCanvas(windowWidth, windowHeight);
   background(30);
@@ -12,4 +14,6 @@ window.draw = function draw() {
   const diameter: number = random(20, 100);
   fill(randomColour());
   circle(mouseX, mouseY, diameter);
+  const vec = p5.Vector.random2D().mult(200);
+  square(vec.x, vec.y, 10);
 };
