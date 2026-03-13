bafc696a843f1f914edee2a66d00f0dec34017f7
nbogie <neillbogie@googlemail.com>
2026-03-13 15:58:41 +0000

    a more interesting first example

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,26 @@ window.setup = function setup() {
 };
 
 window.draw = function draw() {
-  const diameter: number = random(20, 100);
-  fill(randomColour());
-  circle(mouseX, mouseY, diameter);
-  const vec = p5.Vector.random2D().mult(200);
-  square(vec.x, vec.y, 10);
+  drawMyDesign();
 };
+
+//delete this
+function drawMyDesign() {
+  blendMode(SUBTRACT);
+  background(0, 15);
+  blendMode(BLEND);
+  const mouse = createVector(mouseX, mouseY);
+  const pMouse = createVector(pmouseX, pmouseY);
+  const towards = p5.Vector.sub(mouse, pMouse);
+  towards.normalize();
+
+  rectMode(CENTER);
+  noStroke();
+  const radius: number = (random(0.2, 1) * width) / 4;
+  for (let i = 0; i < 10; i++) {
+    fill(randomColour());
+    const offset = p5.Vector.fromAngle(towards.heading() + randomGaussian(0, 0.1)).mult(radius);
+    const vec = offset.add(mouse);
+    square(vec.x, vec.y, 10);
+  }
+}
